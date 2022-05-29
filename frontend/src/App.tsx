4ac8f1a25a60d867c7f02a3394a0e212b27b93a3
dmattia <david@transcend.io>
2022-05-29 13:35:07 -0500

    Test our Create-react-app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,19 @@
-import { useAuth } from "./hooks/use_auth";
-import { LoginPage } from "./components";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({});
 
 function App() {
-  const auth = useAuth();
-
-  if (!auth?.credentials) {
-    return <LoginPage />;
-  }
-
   return (
-    <>
-      <p>Bri and David's RV Website</p>
-      <p>{auth.credentials.accessKeyId}</p>
-      <p>{auth.credentials.secretAccessKey}</p>
-      <p>{auth.credentials.sessionToken}</p>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>RV App</p>
+      </header>
+    </div>
   );
 }
 
